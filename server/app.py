900af6efe744d8d9bbe2be1975ffa3a0e34e4124
Will Tanner <WT58@canterbury.ac.uk>
2026-03-31 21:32:59 +0100

    implementing a simple base html using bootstrap to check navigation features

--- server/app.py
+++ server/app.py
@@ -1,13 +1,18 @@
-from flask import Flask 
+from flask import Flask, render_template
 from database import DataBaseSetup, OpenDBConn
-import sqlite3
+import sqlite3 
 
-app = Flask(__name__)
+app = Flask(
+__name__,
+template_folder="../client/templates",
+static_folder="../client/static"
+)
+
 DataBaseSetup()
 
 @app.route("/")
 def home():
-    return "message: expense api running"
+    return render_template("index.html")
 
 if __name__=='__main__':
     app.run(debug=True)
